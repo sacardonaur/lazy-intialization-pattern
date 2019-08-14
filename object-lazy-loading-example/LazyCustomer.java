6a8beb39ff5969b3096c4d53ae3bf2930c281c7e
sacardonaur <sacardonaur@unal.edu.co>
2019-08-14 10:14:24 -0500

    Update LazyCustomer.java

--- object-lazy-loading-example/LazyCustomer.java
+++ object-lazy-loading-example/LazyCustomer.java
@@ -15,3 +15,17 @@ public class LazyCustomer {
     return orders;
   }
 }
+
+public class NotLazyCustomer {
+  private List<Order> orders;
+  private String name;
+
+  public Customer() {
+    this.name = "Juan";
+    this.orders = LoadOrders();
+  }
+
+  public List<Order> getOrders() {
+    return orders;
+  }
+}

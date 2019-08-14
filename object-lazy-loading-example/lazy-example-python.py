b07ad43a8ba5dc1c514dbdfcf046f1e57fca5abf
sacardonaur <sacardonaur@unal.edu.co>
2019-08-14 11:04:23 -0500

    Update lazy-example-python.py

--- object-lazy-loading-example/lazy-example-python.py
+++ object-lazy-loading-example/lazy-example-python.py
@@ -1,17 +1,17 @@
->>> class MyMetaClass(type):
+>>> class PrinterService(type):
 ...     @property
-...     def my_data(cls):
-...         if getattr(cls, '_MY_DATA', None) is None:
-...             print("costly database call executing")
-...             my_data = 'bar'
-...             cls._MY_DATA = my_data
-...         return cls._MY_DATA
+...     def available_printers(cls):
+...         if getattr(cls, '_AVAILABLE_PRINTERS', None) is None:
+...             print("costly check of printer availability with I/O processes")
+...             available_printers = 'bar'
+...             cls._AVAILABLE_PRINTERS = available_printers
+...         return cls._AVAILABLE_PRINTERS
 ... 
->>> class MyClass(metaclass=MyMetaClass):
+>>> class PrinterService(metaclass=PrinterService):
 ...     pass
 ... 
->>> MyClass.my_data
+>>> MyClass.available_printers
 costly database call executing
 'bar'
->>> MyClass.my_data
+>>> MyClass.available_printers
 'bar'
